62a2a5848fa7f847c5087a7016fba1e115c578d9
Micaiah Skolnick <mskolnick@coolfiresolutions.com>
2020-05-15 12:38:59 -0400

    Add plugin files

---
+++ README.md
@@ -0,0 +1,15 @@
+# zsh_git_helpers
+
+## Description
+
+Oh-my-zsh plugin which provides helpers for common git commands
+
+## Usage
+
+1. Checkout any git branch
+
+        git checkout <branch1>
+
+2. Run Command
+
+        pull_and_merge <any-branch>
---
+++ zsh_git_helpers.plugin.zsh
@@ -0,0 +1,41 @@
+# Helpers for common git operations
+#
+# See README.md for details
+
+alias pull_and_merge="_pull_and_merge"
+
+local current_branch_name branch_to_merge
+
+_merge_entered_branch_to_current() {
+    echo "\nChecking out '$current_branch_name'...\n" &&
+        git checkout $current_branch_name &&
+        echo "\nMerging '$branch_to_merge' into '$current_branch_name'...\n" &&
+        git merge $branch_to_merge
+}
+
+_update_entered_branch() {
+    echo -e "Checking out '$branch_to_merge'...\n"
+
+    if [ "$branch_to_merge" = "" ]; then
+        echo "Error: Name of branch to merge cannot be empty"
+        return 1
+    fi
+
+    git checkout $branch_to_merge &&
+        echo -e "\nPulling '$branch_to_merge'...\n" &&
+        git pull
+}
+
+_read_params() {
+    # This line will echo its own error if not currently in git repo
+    current_branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+    # Store command argument to variable
+    branch_to_merge=$1
+}
+
+_pull_and_merge() {
+    _read_params &&
+        _update_entered_branch &&
+        _merge_entered_branch_to_current
+}
